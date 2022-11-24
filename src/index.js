36a5e678e78fbf12ddf5daaed8e33501cfe2043d
ormide <ormide@live.fr>
2022-11-24 11:58:30 +0100

    header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import "./style.scss";
+import "./assets/styles/styles.scss";
+import "./index.scss";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+
+const canvas = document.querySelector(".canvas");
+const canvasShow = document.querySelector(".toggleCanvas");
+canvasShow.addEventListener("click", (event) => {
+    event.stopPropagation();
+    canvas.setAttribute("class", "canvas show");
+});
+const canvasClose = document.querySelector(".canvasClose");
+canvasClose.addEventListener("click", (event) => {
+    event.stopPropagation();
+    canvas.setAttribute("class", "canvas");
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { dirname } = require("path");
 
 module.exports = {
     entry: {
         main: path.join(__dirname, "src/index.js"),
+        form: path.join(__dirname, "src/form/form.js"),
+        topbar: path.join(__dirname, "src/assets/javascript/topbar.js"),
     },
     output: {
         path: path.join(__dirname, "dist"),
@@ -17,14 +22,34 @@ module.exports = {
                 use: ["babel-loader"],
             },
             {
-                test: /\.scss$/i,
+                test: /\.(sass|scss|css)$/i,
                 use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(svg|eot|woff|woff2|ttf)$/,
+                use: ["file-loader"],
             },
         ],
     },
     plugins: [
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: "./src/assets/images/*",
+                    to: "assets/images/[name][text]",
+                },
+            ],
+        }),
         new HtmlWebpackPlugin({
+            filename: "index.html",
             template: path.join(__dirname, "./src/index.html"),
+            chunks: ["main", "topbar"],
+        }),
+        new HtmlWebpackPlugin({
+            filename: "form.html",
+            template: path.join(__dirname, "./src/form/form.html"),
+            chunks: ["form", "topbar"],
         }),
     ],
     stats: "minimal",
