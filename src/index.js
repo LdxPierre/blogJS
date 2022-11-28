34e247796ce52133cedfe76822b08a57a0b55c0c
ormide <ormide@live.fr>
2022-11-28 16:54:22 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -1,101 +1,151 @@
-import "./assets/styles/styles.scss";
 import "./index.scss";
+import "./assets/styles/styles.scss";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 
-const showResult = (data) => {
-    const posts = document.querySelector(".posts");
-    data.forEach((d) => {
-        const post = document.createElement("div");
-        post.setAttribute("class", "post");
-        post.innerHTML = `
-        <h2>${d.title}</h2>
-        <h3>Auteur : ${d.userId}</h3>
-        <p>${d.body}</p>
-        <a href="#">Read</a>`;
-        const deleteBtn = document.createElement("span");
-        deleteBtn.setAttribute("class", "deletePost");
-        deleteBtn.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
-        deleteBtn.addEventListener("click", (event) => {
-            event.stopPropagation();
-            deletePost(d.id);
-        });
-        post.addEventListener("click", () => {
-            getPost(d.id);
-        });
-        posts.append(post);
-        post.append(deleteBtn);
-    });
-};
+const contentNode = document.querySelector(".content");
 
-const showError = (err) => {
-    console.log(err);
-};
-
-const toggleSpinner = (value) => {
-    document.querySelector(".posts").innerHTML = ``;
-    if (value === true) {
-        const content = document.querySelector(".content");
-        const spinner = document.createElement("span");
-        spinner.setAttribute("class", "spinner");
-        spinner.innerHTML = `<i class="fa-solid fa-gear"></i>`;
-        content.append(spinner);
-    } else {
-        document.querySelector(".spinner").remove();
-    }
-};
-
-async function request() {
-    toggleSpinner(true);
+//show all posts
+const getPosts = async () => {
     try {
-        fetch("https://jsonplaceholder.typicode.com/posts")
-            .then((response) => response.json())
-            .then((json) => showResult(json.reverse()));
+        const response = await fetch(
+            "https://jsonplaceholder.typicode.com/posts"
+        );
+        const body = await response.json();
+        showPosts(body);
     } catch (e) {
-        showError(e);
-    } finally {
-        toggleSpinner(false);
+        console.error(e);
     }
-}
-
-async function deletePost(id) {
-    try {
-        fetch("https://jsonplaceholder.typicode.com/posts/" + id, {
-            method: "DELETE",
-        })
-            .then(() => console.log("post : " + id + "delete"))
-            .catch((e) => console.log(e));
-    } catch {
-        console.log("failed");
-    }
-}
-
-const getPost = (id) => {
-    const post = fetch("https://jsonplaceholder.typicode.com/posts/" + id)
-        .then((response) => response.json())
-        .then((json) => togglePost(json))
-        .catch((e) => console.log(e));
 };
 
-const togglePost = (p) => {
-    const posts = document.querySelector(".posts");
-    posts.innerHTML = `
-    <ul>
-        <li>${p.id}</li>
-        <li>${p.title}</li>
-        <li>${p.userId}</li>
-        <li>${p.body}</li>
-    </ul>
-    `;
+//show post[id]
+const getPost = async (id) => {
+    try {
+        const response = await fetch(
+            `https://jsonplaceholder.typicode.com/posts/${id}`
+        );
+        const body = await response.json();
+        showPost(body);
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+//delete post[id]
+const deletePost = async (id) => {
+    try {
+        const response = await fetch(
+            `https://jsonplaceholder.typicode.com/posts/${id}`,
+            {
+                method: "DELETE",
+            }
+        );
+        const body = await response.json();
+        console.log("delete article : " + body);
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+//show post[id]/Comments
+const getComments = async (id) => {
+    try {
+        const response = await fetch(
+            `https://jsonplaceholder.typicode.com/posts/${id}/comments`
+        );
+        const body = await response.json();
+        showComments(body);
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+const showPosts = (posts) => {
+    contentNode.innerHTML = ``;
+    const postsNode = document.createElement("div");
+    postsNode.setAttribute("class", "posts");
+    posts.forEach((p) => {
+        const post = createPost(p);
+        postsNode.append(post);
+    });
+    contentNode.append(postsNode);
+};
+
+const createPost = (p) => {
+    //post
+    const postNode = document.createElement("div");
+    postNode.setAttribute("class", "post");
+    postNode.innerHTML = `
+    <h2>${p.title}</h2>
+    <h3>Auteur : ${p.userId}</h3>
+    <p>${p.body}</p>
+    <a href="#">Read</a>`;
+    postNode.addEventListener("click", () => {
+        getPost(p.id);
+    });
+    //button delete
+    const deleteBtn = document.createElement("span");
+    deleteBtn.setAttribute("class", "deletePost");
+    deleteBtn.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+    deleteBtn.addEventListener("click", (event) => {
+        event.stopPropagation();
+        deletePost(p.id);
+    });
+    postNode.append(deleteBtn);
+
+    return postNode;
+};
+
+const showPost = (p) => {
+    contentNode.innerHTML = ``;
     const returnToPostsBtn = document.createElement("button");
+    returnToPostsBtn.innerHTML = `Back`;
     returnToPostsBtn.setAttribute("class", "btn");
     returnToPostsBtn.addEventListener("click", () => {
-        returnToPosts();
+        getPosts();
     });
-    posts.append(returnToPostsBtn);
-    const returnToPosts = () => {
-        request();
-    };
+    const postNode = document.createElement("div");
+    postNode.setAttribute("class", "post");
+    postNode.innerHTML = `
+    <span>id : ${p.id}</span>
+    <span>Author : ${p.userId}</span>
+    <span>Title : ${p.title}</span>
+    <span>Body : ${p.body}</span>
+    `;
+    contentNode.append(returnToPostsBtn, postNode);
+    getComments(p.id);
 };
 
-request();
+const showComments = (comments) => {
+    const ul = document.createElement("ul");
+    comments.forEach((c) => {
+        const li = createComment(c);
+        ul.append(li);
+    });
+    document.querySelector(".post").after(ul);
+};
+
+const createComment = (c) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+    <span>id : ${c.id}</span>
+    <span>name : ${c.name}</span>
+    <span>email : ${c.email}</span>
+    <span>body : ${c.body}</span>
+    `;
+    return li;
+};
+
+// const toggleSpinner = (value) => {
+//     if (value === true) {
+//         const contentNode = document.querySelector(".content");
+//         const spinnerNode = document.createElement("span");
+//         spinnerNode.setAttribute("class", "spinner");
+//         spinnerNode.innerHTML = `<i class="fa-solid fa-gear"></i>`;
+//         contentNode.append(spinnerNode);
+//     } else {
+//         document.querySelector(".spinner").remove();
+//     }
+// };
+
+getPosts();
