6971be7d49056ea1757f22a44fbe309e81e14b1d
Ormide <ormide@live.fr>
2022-12-08 15:32:15 +0100

    filterImproved

--- src/index.js
+++ src/index.js
@@ -6,155 +6,136 @@ import * as popUp from "./assets/javascript/popup.js";
 import * as modal from "./assets/javascript/modal.js";
 
 const params = new URL(document.location).searchParams;
-const contentNode = document.querySelector(".content");
-let filtersArray = [];
-let sort = "desc";
+const categoriesListNode = document.querySelector(".categoriesList");
+let articles;
+let filter;
+let sort = "createdAt:desc";
 let categoryShow = false;
 
-//show all posts
-const getPosts = async () => {
-    let url = new URL("https://restapi.fr/api/ormide/");
-    if (filtersArray != 0) {
-        filtersArray.forEach((f) => {
-            url.searchParams.append("category", f);
-        });
-    }
-    sort == "asc"
-        ? url.searchParams.append("sort", "createdAt:asc")
-        : url.searchParams.append("sort", "createdAt:desc");
+//show all articles
+const getArticles = async () => {
     try {
-        const response = await fetch(url);
-        const body = await response.json();
-        showPosts(body);
+        const response = await fetch(
+            `https://restapi.fr/api/ormide?sort=${sort}`
+        );
+        articles = await response.json();
+        showArticles();
+        categoriesMenu();
     } catch (e) {
         console.error(e);
     }
 };
 
-//delete post[id]
-const deletePost = async (id) => {
+//delete article[id]
+const deleteArticle = async (id) => {
     try {
         const response = await fetch(`https://restapi.fr/api/ormide/${id}`, {
             method: "DELETE",
         });
-        const body = response.json();
-        getPosts();
+        const body = await response.json();
+        getArticles();
         popUp.showPopUp("success", "Article has been deleted");
     } catch (e) {
         console.error(e);
     }
 };
 
-const showFilters = () => {
-    const filtersNode = document.createElement("div");
-    filtersNode.classList.add("filters");
-    filtersNode.innerHTML = `
-        <div class="category-list">
-            <label for="catA">
-                Category A
-                <input type="checkbox" name="catA" id="catA" value="A"/>
-            </label>
-            <label for="catB">
-                Category B
-                <input type="checkbox" name="catB" id="catB" value="B"/>
-            </label>
-            <label for="catC">
-                Category C
-                <input type="checkbox" name="catC" id="catC" value="C"/>
-            </label>
-            <label for="catD">
-                Category D
-                <input type="checkbox" name="catD" id="catD" value="D"/>
-            </label>
-        </div>
-    `;
+const categoriesMenu = () => {
+    if (articles.length == 0) {
+        categoriesListNode.innerHTML = ``;
+    } else if (articles.length > 0) {
+        let categoriesArray;
+        const categories = articles.reduce((acc, article) => {
+            acc[article.category]
+                ? acc[article.category]++
+                : (acc[article.category] = 1);
+            return acc;
+        }, {});
 
-    const filterSelectNode = document.createElement("div");
-    filterSelectNode.classList.add("filter-select");
+        categoriesArray = Object.keys(categories)
+            .map((category) => {
+                return [category, categories[category]];
+            })
+            .sort((c1, c2) => c1[0].localeCompare(c2[0]));
 
-    const sortDateNode = sortDate();
-    const filterCategoryNode = filterCategory();
-
-    filterSelectNode.prepend(sortDateNode, filterCategoryNode);
-    filtersNode.prepend(filterSelectNode);
-    contentNode.prepend(filtersNode);
-
-    filtersInput();
+        createLiElements(categoriesArray);
+    } else {
+        createLiElements([[articles.category, 1]]);
+    }
 };
 
-const sortDate = () => {
-    const sortDateNode = document.createElement("span");
-    sortDateNode.classList.add("sort-date");
-    sortDateNode.innerHTML = `
-    <span>Sort by date</span>
-    <span class='filter-ico'><i class="fa-solid fa-angle-up"></i></span>
-    `;
-    sortDateNode.addEventListener("click", () => {
-        sort == "desc" ? (sort = "asc") : (sort = "desc");
-        sort == "desc"
-            ? sortDateNode.setAttribute("class", "sort-date up")
-            : sortDateNode.setAttribute("class", "sort-date down");
-        getPosts();
+const createLiElements = (categoriesArray) => {
+    categoriesListNode.innerHTML = ``;
+    const liElements = categoriesArray.map((element) => {
+        const li = document.createElement("li");
+        li.innerHTML = `${element[0]} ( ${element[1]} )`;
+        filter === element[0] ? li.classList.add("select") : "";
+        li.addEventListener("click", () => {
+            liElements.forEach((li) => {
+                li.classList.remove("select");
+            });
+            if (filter === element[0]) {
+                filter = null;
+            } else {
+                li.classList.add("select");
+                filter = element[0];
+            }
+            showArticles();
+        });
+        return li;
     });
-    return sortDateNode;
+
+    categoriesListNode.append(...liElements);
 };
 
-const filterCategory = () => {
-    const filterCategoryNode = document.createElement("span");
-    filterCategoryNode.classList.add("filter-category");
-    filterCategoryNode.innerHTML = `
-    Filter by category
-    <span class='filter-ico'><i class="fa-solid fa-angle-up"></i></span>
-    `;
-    filterCategoryNode.addEventListener("click", () => {
-        const categoryListNode = document.querySelector(".category-list");
+const filterEventListener = () => {
+    const sortDateNode = document.querySelector(".sortDate");
+    sortDateNode.addEventListener("click", () => {
+        sort == "createdAt:asc"
+            ? (sort = "createdAt:desc")
+            : (sort = "createdAt:asc");
+        sort == "createdAt:asc"
+            ? sortDateNode.setAttribute("class", "sortDate on")
+            : sortDateNode.setAttribute("class", "sortDate off");
+        getArticles();
+    });
+
+    const filterCategoriesNode = document.querySelector(".filterCategories");
+    const filterCategoriesListNode = document.querySelector(".categoriesList");
+    filterCategoriesNode.addEventListener("click", () => {
         if (!categoryShow) {
-            categoryListNode.classList.add("show");
-            filterCategoryNode.setAttribute("class", "filter-category down");
+            filterCategoriesNode.setAttribute("class", "filterCategories on");
+            filterCategoriesListNode.classList.add("show");
             categoryShow = !categoryShow;
         } else {
-            categoryListNode.classList.remove("show");
-            filterCategoryNode.setAttribute("class", "filter-category up");
+            filterCategoriesNode.setAttribute("class", "filterCategories off");
+            filterCategoriesListNode.classList.remove("show");
             categoryShow = !categoryShow;
         }
     });
-
-    return filterCategoryNode;
 };
 
-const filtersInput = () => {
-    const categoryListNode = document.querySelector(".category-list");
-    const cats = categoryListNode.querySelectorAll("input");
-    cats.forEach((c) => {
-        c.addEventListener("click", () => {
-            if (c.checked) {
-                filtersArray.push(c.value);
-                getPosts();
-            } else {
-                const index = filtersArray.indexOf(c.value);
-                filtersArray.splice(index, 1);
-                getPosts();
-            }
-        });
-    });
-    return filtersArray;
-};
-
-const showPosts = (posts) => {
+const showArticles = () => {
     const articlesNode = document.querySelector(".articles");
     articlesNode.innerHTML = ``;
-    //if data
-    if (posts.length != 0) {
-        //if multiple data
-        if (posts.length > 0) {
-            posts.map((p) => {
-                const post = createPost(p);
-                articlesNode.append(post);
-            });
-            //if only one data, no array to iterate with .map
+
+    const articlesFilter = articles.filter((article) => {
+        if (filter) {
+            return article.category === filter;
         } else {
-            const post = createPost(posts);
-            articlesNode.append(post);
+            return true;
+        }
+    });
+
+    if (articlesFilter.length != 0) {
+        if (articlesFilter.length > 0) {
+            articlesFilter.map((a) => {
+                const article = createArticle(a);
+                articlesNode.append(article);
+            });
+        } else {
+            const article = createArticle(articlesFilter);
+            articlesNode.append(article);
         }
     } else {
         //NO DATA
@@ -162,55 +143,51 @@ const showPosts = (posts) => {
     }
 };
 
-const createPost = (p) => {
-    const date = new Date(p.createdAt);
+const createArticle = (a) => {
+    const date = new Date(a.createdAt);
     const dateIso = date.toLocaleString();
     //title, cat, content
-    const postNode = document.createElement("div");
-    postNode.classList.add("article");
-    postNode.innerHTML = `<h2 class="articleTitle">${p.title}</h2>
-        <h4 class="articleCategory">${p.category}</h4>
-        <p class="articleContent">${p.content}</p>`;
+    const articleNode = document.createElement("div");
+    articleNode.classList.add("article");
+    articleNode.innerHTML = `<h2 class="articleTitle">${a.title}</h2>
+        <h4 class="articleCategory">${a.category}</h4>
+        <p class="articleContent">${a.content}</p>`;
     //author, date, btn
     const articlesInfosNode = document.createElement("div");
     articlesInfosNode.classList.add("articleInfos");
     articlesInfosNode.innerHTML = `
-            <img class="articleAuthorImg" src="${p.authorImg}" alt="${p.author}"/>
+            <img class="articleAuthorImg" src="${a.authorImg}" alt="${a.author}"/>
             <h5 class="articleDate">${dateIso}</h5>
-            <h4 class="articleAuthor">${p.author}</h4>
+            <h4 class="articleAuthor">${a.author}</h4>
             <button class='btn btn-read'>Read</button>
             `;
-    postNode.append(articlesInfosNode);
+    articleNode.append(articlesInfosNode);
     //btnDelete
     const btnDelete = document.createElement("button");
     btnDelete.setAttribute("class", "btn btn-danger btn-delete");
     btnDelete.innerHTML = `Delete`;
     btnDelete.addEventListener("click", () => {
         modal.showModal({
-            content: `Confirm delete of ${p.title} ? `,
+            content: `Confirm delete of ${a.title} ? `,
             footer: {
-                method: deletePost,
-                params1: p._id,
+                method: deleteArticle,
+                params1: a._id,
             },
         });
     });
     //editBtn
     const btnEdit = document.createElement("a");
     btnEdit.setAttribute("class", "btn btn-blue btn-edit");
-    btnEdit.setAttribute("href", `./form.html?id=${p._id}`);
+    btnEdit.setAttribute("href", `./form.html?id=${a._id}`);
     btnEdit.innerHTML = "Edit";
 
     articlesInfosNode.append(btnEdit, btnDelete);
 
-    return postNode;
+    return articleNode;
 };
 
-getPosts();
-showFilters();
-
-const test = (param) => {
-    console.log(param);
-};
+getArticles();
+filterEventListener();
 
 //Show popUp (must be improved)
 if (params.get("p") == "create") {
