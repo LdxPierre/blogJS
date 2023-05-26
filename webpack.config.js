efe012317c41fec73403366b2baf5fa7cb5a1082
Pierre <pierre.ledoux@outlook.fr>
2023-05-26 10:08:30 +0200

    remove images dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const { dirname } = require("path");
 
@@ -37,14 +36,6 @@ module.exports = {
 	},
 	plugins: [
 		new CleanWebpackPlugin(),
-		new CopyWebpackPlugin({
-			patterns: [
-				{
-					from: "./src/assets/images",
-					to: "assets/images",
-				},
-			],
-		}),
 		new HtmlWebpackPlugin({
 			filename: "index.html",
 			template: path.join(__dirname, "./src/index.html"),
@@ -57,8 +48,7 @@ module.exports = {
 		}),
 	],
 	stats: "minimal",
-	devtool: "source-map",
-	mode: "development",
+	mode: "production",
 	devServer: {
 		open: false,
 		static: path.resolve(__dirname, "./dist"),
